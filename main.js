0c0f72681914b7ac9c635a35795e3dd1a034b28e
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2021-10-09 20:41:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ function preload()
 
 function setup()
 {
-    canvas = createCanvas(700, 700);
+    canvas = createCanvas(300, 300, 1000, 400);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw()
@@ -19,3 +25,17 @@ function make_moustache()
     save("My Moustache Maker Image!.png");
 }
 
+function modelLoaded()
+{
+    console.log("PoseNet has been successfully initialized!");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);        
+    }
+}
